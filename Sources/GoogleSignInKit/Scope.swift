9959f539ea9472a2e606332e189d25ace4620519
Thibault Le Cornec <lecornec.thibault@me.com>
2025-04-06 05:45:28 +0200

    Change Scope type to a struct to make possible to create custom scope

--- Sources/GoogleSignInKit/Scope.swift
+++ Sources/GoogleSignInKit/Scope.swift
@@ -9,20 +9,19 @@ import Foundation
 
 extension GoogleSignInKit {
     
-    public enum Scope {
-        case email
-        case openID
-        case profile
-        
-        internal var rawValue: String {
-            switch self {
-            case .email: return "email"
-            case .openID: return "openid"
-            case .profile: return "profile"
-            }
+    public struct Scope: RawRepresentable, Equatable, Hashable, Codable {
+        public let rawValue: String
+
+        public init(rawValue: String) {
+            self.rawValue = rawValue
         }
+
+        // Basics scopes
+        public static let email = Scope(rawValue: "email")
+        public static let openID = Scope(rawValue: "OpenID")
+        public static let profile = Scope(rawValue: "profile")
     }
-    
+
     internal enum ScopeURL: String {
         case email = "https://www.googleapis.com/auth/userinfo.email"
         case openID = "openid"
